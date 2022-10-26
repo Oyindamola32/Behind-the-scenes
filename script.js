3d9c6178cc1930e8e1b1fc504310269de80dd187
Oyindamola32 <oyindamolatolulope82@gmail.com>
2022-10-26 10:40:01 +0100

    modify script.js

--- script.js
+++ script.js
@@ -165,3 +165,17 @@ const jonas = {
 
 jonas.greet();
 jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
+};
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
+
+var addArrow = (a, b) => {
+  console.log(arguments);
+  return a + b;
+};
+addArrow(2, 5, 8);
